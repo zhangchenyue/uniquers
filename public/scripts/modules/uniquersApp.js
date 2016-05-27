b613adbebff34e5b33c8790fffc95c4929aeb19d
Chen Yue Zhang <czhang13@slb.com>
2016-05-27 18:58:53 -0500

    #11 adjust the animate effect

--- public/scripts/modules/uniquersApp.js
+++ public/scripts/modules/uniquersApp.js
@@ -8,17 +8,32 @@ angular.module('uniquers.controllers', [
 ]);
 
 
+
+angular.module('uniquers.app',['ngRoute']).run('$rootScope', function ($rootScope) {
+    // publish current transition direction on rootScope
+    $rootScope.direction = 'ltr';
+    // listen change start events
+    $rootScope.$on('$routeChangeStart', function (event, next, current) {
+        $rootScope.direction = 'rtl';
+        // console.log(arguments);
+        if (current && next && (current.depth > next.depth)) {
+            $rootScope.direction = 'ltr';
+        }
+    });
+});
+
+
 angular.module('uniquers.app', [
     'uniquers.controllers',
     'uniquers.services',
     'uniquers.filters',
     'uniquers.directives',
- 	'ngRoute',
+    'ngRoute',
     'ngAnimate'
 ]).config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
     $routeProvider
-        .when('/', {templateUrl: '/views/welcome.html', controller: 'WelcomeCtrl'})
-        .when('/home', {templateUrl: '/views/home.html', controller: 'HomeCtrl'})
-        .otherwise({redirectTo: '/'});
+        .when('/', { templateUrl: '/views/welcome.html', controller: 'WelcomeCtrl' })
+        .when('/home', { templateUrl: '/views/home.html', controller: 'HomeCtrl' })
+        .otherwise({ redirectTo: '/' });
     $locationProvider.html5Mode(true);
 }]);
