a9438ccaf78869457c495d23c71b150a43140812
Chen Yue Zhang <czhang13@slb.com>
2016-05-27 02:10:17 -0500

    #6 submit omit files

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,77 @@ var gulp = require('gulp'),
   nodemon = require('gulp-nodemon'),
   plumber = require('gulp-plumber'),
   livereload = require('gulp-livereload');
+rimraf = require('rimraf'),
+  concat = require('gulp-concat'),
+  cssmin = require('gulp-cssmin'),
+  uglify = require('gulp-uglify');
 
+var basePath = './public'
+var jsLibBundle = basePath + '/scripts/minlib.js';
+var jsSiteBundle = basePath + '/scripts/minsite.js';
+var cssLib = basePath + '/styles/lib/*.css'
+var cssSite = basePath + '/styles/*.css'
+var cssLibBundle = basePath + '/styles/lib/min-lib.css'
+var cssSiteBundle = basePath + '/styles/min-site.css'
+
+gulp.task('clean:minlibjs', function (cb) {
+  rimraf(jsLibBundle, cb);
+})
+
+gulp.task('clean:minsitejs', function (cb) {
+  rimraf(jsSiteBundle, cb);
+})
+
+gulp.task('clean:minlibcss', function (cb) {
+  rimraf(cssLibBundle, cb);
+});
+
+gulp.task('clean:minsitecss', function (cb) {
+  rimraf(cssSiteBundle, cb);
+})
+
+gulp.task('min:libjs', function () {
+  gulp.src([
+    'public/scripts/lib/jquery.min.js', 
+    'public/scripts/lib/bootstrap.min.js',
+    'public/scripts/lib/angular.js',
+    'public/scripts/lib/angular-route.min.js',
+    'public/scripts/lib/angular-annimate.min.js'
+  ], { base: '.' })
+    .pipe(concat('minlib.js'))
+    //.pipe(uglify())
+    .pipe(gulp.dest('public/scripts/'));
+});
+
+gulp.task('min:sitejs', function () {
+  gulp.src([
+    'public/scripts/common/*.js',
+    'public/scripts/modules/*.js',
+    'public/scripts/controllers/*.js',
+    'public/scripts/directives/*.js',
+    'public/scripts/effects/*.js',
+    'public/scripts/filters/*.js',
+    'public/scripts/services/*.js'
+
+  ], { base: '.' })
+    .pipe(concat('minsite.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/scripts/'));
+});
+
+gulp.task('min:libcss', function () {
+  gulp.src([cssLib], { base: '.' })
+    .pipe(concat('min-lib.css'))
+    .pipe(cssmin())
+    .pipe(gulp.dest('public/styles/lib/'));
+});
+
+gulp.task('min:sitecss', function () {
+  gulp.src([cssSite], { base: '.' })
+    .pipe(concat('min-site.css'))
+    .pipe(cssmin())
+    .pipe(gulp.dest('public/styles/'));
+});
 
 gulp.task('develop', function () {
   livereload.listen();
@@ -12,7 +82,7 @@ gulp.task('develop', function () {
     stdout: false
   }).on('readable', function () {
     this.stdout.on('data', function (chunk) {
-      if(/^Express server listening on port/.test(chunk)){
+      if (/^Express server listening on port/.test(chunk)) {
         livereload.changed(__dirname);
       }
     });
@@ -22,5 +92,13 @@ gulp.task('develop', function () {
 });
 
 gulp.task('default', [
+  'clean:minlibjs',
+  'clean:minsitejs',
+  'clean:minlibcss',
+  'clean:minsitecss',
+  'min:libjs',
+    'min:sitejs',
+  'min:libcss',
+  'min:sitecss',
   'develop'
 ]);
