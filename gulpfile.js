6d8def761149e96b6fab127a1f0ca33cbf2f9545
Chen Yue Zhang <CZhang13@slb.com>
2016-07-07 16:04:38 +0800

    modified gulp scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,76 +1,89 @@
+//plugins
 var gulp = require('gulp'),
   nodemon = require('gulp-nodemon'),
-  plumber = require('gulp-plumber'),
   livereload = require('gulp-livereload'),
   rimraf = require('rimraf'),
   concat = require('gulp-concat'),
   cssmin = require('gulp-cssmin'),
   uglify = require('gulp-uglify'),
-  rev = require('gulp-rev'),                                //- 对文件名加MD5后缀
-  revCollector = require('gulp-rev-collector'),           //- 路径替换
+  rev = require('gulp-rev'),
+  revCollector = require('gulp-rev-collector'),
   browserSync = require('browser-sync');
 
-//clean javascript task
-gulp.task('clean:minjs', function (cb) {
-  rimraf('./public/dist/min-*.js', cb);
-})
+//production files
+var min = {
+  'js': './public/dist/min-*.js',
+  'css': './public/dist/min-*.css'
+};
 
-//clean css task
-gulp.task('clean:mincss', function (cb) {
-  rimraf('./public/dist/min-*.css', cb);
-});
+//source files
+var srcJS = [
+  'public/scripts/lib/jquery.min.js',
+  'public/scripts/lib/bootstrap.min.js',
+  'public/scripts/lib/angular.min.js',
+  'public/scripts/lib/angular-route.min.js',
+  'public/scripts/lib/angular-animate.min.js',
+  'public/scripts/common/*.js',
+  'public/scripts/modules/*.js',
+  'public/scripts/controllers/*.js',
+  'public/scripts/directives/*.js',
+  'public/scripts/effects/*.js',
+  'public/scripts/filters/*.js',
+  'public/scripts/services/*.js'
+];
 
-//minify javascript files
+var srcCSS = [
+  './public/styles/animate.css',
+  './public/styles/bootstrap.min.css',
+  './public/styles/fonts.css',
+  './public/styles/style.css',
+];
+
+/*--------------------Tasks-------------------*/
+
+gulp.task('clean:minjs', function (cb) { rimraf(min['js'], cb); })
+
+gulp.task('clean:mincss', function (cb) { rimraf(min['css'], cb); });
+
 gulp.task('min:js', ['clean:minjs'], function () {
-  gulp.src([
-    'public/scripts/lib/jquery.min.js',
-    'public/scripts/lib/bootstrap.min.js',
-    'public/scripts/lib/angular.min.js',
-    'public/scripts/lib/angular-route.min.js',
-    'public/scripts/lib/angular-animate.min.js',
-    'public/scripts/common/*.js',
-    'public/scripts/modules/*.js',
-    'public/scripts/controllers/*.js',
-    'public/scripts/directives/*.js',
-    'public/scripts/effects/*.js',
-    'public/scripts/filters/*.js',
-    'public/scripts/services/*.js'
-  ], { base: '.' })
+  gulp.src(srcJS, { base: '.' })
     .pipe(concat('min.js'))
     .pipe(uglify())
-    .pipe(rev())                                            //- 文件名加MD5后缀
+    .pipe(rev())
     .pipe(gulp.dest('./public/dist/'))
     .pipe(rev.manifest())
     .pipe(gulp.dest('rev/js'));
 });
 
-//minify css files
 gulp.task('min:css', ['clean:mincss'], function () {
-  gulp.src([
-    './public/styles/animate.css',
-    './public/styles/bootstrap.min.css',
-    './public/styles/fonts.css',
-    './public/styles/style.css',
-  ], { base: '.' })
+  gulp.src(srcCSS, { base: '.' })
     .pipe(concat('min.css'))
     .pipe(cssmin())
-    .pipe(rev())                                            //- 文件名加MD5后缀
+    .pipe(rev()) 
     .pipe(gulp.dest('./public/dist/'))
     .pipe(rev.manifest())
     .pipe(gulp.dest('rev/css'));
 });
 
-//clean javascript and css
+
 gulp.task('clean', [
   'clean:minjs',
   'clean:mincss'
 ]);
 
-//minify javascript and css files and merge to one file
 gulp.task('min', [
   'min:js',
   'min:css'
 ]);
+
+//support rewrite url in html
+gulp.task('rev', function () {
+  gulp.src(['./rev/**/*.json', './public/index.html'])
+    .pipe(revCollector({
+      replaceReved: true
+    }))
+    .pipe(gulp.dest('./public/'));
+});
 
 //for developer use support livereload
 gulp.task('develop', function () {
@@ -101,14 +114,12 @@ gulp.task('browser-sync', ['develop'], function () {
   });
 });
 
-//support rewrite url in html
-gulp.task('rev', function () {
-  gulp.src(['./rev/**/*.json', './public/index.html'])
-    .pipe(revCollector({
-      replaceReved: true
-    }))
-    .pipe(gulp.dest('./public/'));
-});
+
+gulp.task('deploy', [
+  'clean',
+  'min',
+  'rev'
+]);
 
 //final task
 gulp.task('default', [
