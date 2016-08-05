c038b4bb7fa69e0748258441064b55fd90894afc
ChenYue Zhang <CZhang13@slb.com>
2016-08-05 13:56:22 +0800

    adjust code format

--- public/scripts/controllers/AboutCtrl.js
+++ public/scripts/controllers/AboutCtrl.js
@@ -1,8 +1,8 @@
 angular.module('uniquers.controllers').controller('AboutCtrl', [
     '$scope',
     '$rootScope',
-    function ($scope,$rootScope) {
+    function($scope, $rootScope) {
         $scope.username = 'About';
-      
+
     }
 ]);
