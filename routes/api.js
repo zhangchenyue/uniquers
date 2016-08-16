2321e16acc5a78b51d668d158f62ebc766a2a4ad
ChenYue Zhang <CZhang13@slb.com>
2016-08-16 13:44:17 +0800

    add qq strage

--- routes/api.js
+++ routes/api.js
@@ -1,33 +1,70 @@
 var express = require('express');
 var apiRouter = express.Router();
 var db = require('../database/data');
-/* GET users listing. */
+var passport = require('passport')
+var util = require('util')
+var crypto = require('crypto')
 
-apiRouter.get('/api/version', function (req, res) {
-  res.json({ 'version': '1.0.0.0' });
+
+
+
+
+apiRouter.get('/api/version', function(req, res) {
+    res.json({
+        'version': '1.0.0.0'
+    });
 });
 
-apiRouter.get('/api/items', function (req, res) {
-  res.json(db);
-})
-
-apiRouter.get('/api/items/:type', function (req, res) {
-  var type = req.params.type;
-  if (!type) {
+apiRouter.get('/api/items', function(req, res) {
     res.json(db);
-  } else {
-    res.json(db.filter(function (item) {
-      return item.type === type;
-    }));
-  }
 })
 
-apiRouter.get('/api/item/:id', function (req, res) {
-  var id = req.params.id;
-  console.log(id);
-  res.json(db.filter(function (item) {
-      return item.id == id;
+apiRouter.get('/api/items/:type', function(req, res) {
+    var type = req.params.type;
+    if (!type) {
+        res.json(db);
+    } else {
+        res.json(db.filter(function(item) {
+            return item.type === type;
+        }));
+    }
+})
+
+apiRouter.get('/api/item/:id', function(req, res) {
+    var id = req.params.id;
+    console.log(id);
+    res.json(db.filter(function(item) {
+        return item.id == id;
     }));
 })
+
+// GET /auth/qq
+// QQ登录认证时 `state` 为必填参数
+// 系client端的状态值，用于第三方应用防止CSRF攻击，成功授权后回调时会原样带回
+apiRouter.get('/api/auth/qq', function(req, res, next) {
+    req.session = req.session || {};
+    req.session.authState = crypto.createHash('sha1')
+        .update(-(new Date()) + '')
+        .digest('hex');
+    passport.authenticate('qq', {
+        state: req.session.authState
+    })(req, res, next);
+});
+
+// GET /auth/qq/callback
+// 通过比较认证返回的`state`状态值与服务器端`session`中的`state`状态值
+// 决定是否继续本次授权
+apiRouter.get('/api/auth/qq/callback', function(req, res, next) {
+    if (req.session && req.session.authState && req.session.authState === req.query.state) {
+        passport
+            .authenticate('qq', {
+                failureRedirect: '/'
+            })(req, res, next);
+    } else {
+        return next(new Error('Auth State Mismatch'));
+    }
+}, function(req, res) {
+    res.redirect('/');
+});
 
 module.exports = apiRouter;
--- server.js
+++ server.js
@@ -8,6 +8,34 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var TqqStrategy = require('passport-tqq/').Strategy;
+
+var QQ_APP_ID = '101332510'
+var QQ_APP_KEY = 'c138d5bc7f7d806a649bf4400715b698';
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+});
+
+passport.use(new TqqStrategy({
+    clientID: QQ_APP_ID,
+    clientSecret: QQ_APP_KEY,
+    callbackURL: 'http://uniquers.azurewebsites.net/auth/qq/callback'
+},
+    function (accessToken, refreshToken, profile, done) {
+        // asynchronous verification, for effect...
+        process.nextTick(function () {
+            console.log('accessToken', accessToken);
+            console.log('refreshToken', refreshToken);
+            console.log('profile', profile);
+            return done(null, profile);
+        });
+    }
+));
 
 var server = express();
 var env = process.env.NODE_ENV || 'development';
@@ -21,6 +49,8 @@ server.use(bodyParser.urlencoded({
     extended: true
 }));
 
+server.use(passport.initialize());
+server.use(passport.session());
 server.use(require('./routes/api'));
 server.use(require('./routes/page'));
 
@@ -52,7 +82,7 @@ if (server.get('env') === 'development') {
 // no stacktraces leaked to user
 server.use(function (err, req, res, next) {
     res.status(err.status || 500);
-    console.log(err.status + err.message + + req.url);
+    console.log(err.status + err.message + +req.url);
     res.end();
 });
 
