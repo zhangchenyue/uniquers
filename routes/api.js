dbef6e83f92e6d83cb393e20d77e35b6483f5cd8
ChenYue Zhang <CZhang13@slb.com>
2016-08-17 03:45:22 +0800

    add param

--- routes/api.js
+++ routes/api.js
@@ -55,7 +55,7 @@ apiRouter.get('/api/auth/qq', function(req, res, next) {
 // 通过比较认证返回的`state`状态值与服务器端`session`中的`state`状态值
 // 决定是否继续本次授权
 apiRouter.get('/api/auth/qq/callback', function(req, res, next) {
-  res.redirect('/home');
+  res.redirect('/home#' + JSON.stringify(res));
     // if (req.session && req.session.authState && req.session.authState === req.query.state) {
     //     passportAuth
     //         .authenticate('qq', {
