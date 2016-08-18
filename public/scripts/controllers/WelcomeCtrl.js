2c0c9496c636d732803bb7929f7adb3c6654eb27
ChenYue Zhang <CZhang13@slb.com>
2016-08-18 12:50:40 +0800

    add login

--- public/scripts/controllers/WelcomeCtrl.js
+++ public/scripts/controllers/WelcomeCtrl.js
@@ -4,17 +4,20 @@ angular.module('uniquers.controllers').controller('WelcomeCtrl', [
     '$timeout',
     '$location',
     'ajaxProxy',
-    function ($scope,$rootScope,$timeout,$location, ajaxProxy) {
+    function ($scope, $rootScope, $timeout, $location, ajaxProxy) {
         $scope.content = {
-            text1:'Welcome',
-            text2:'to',
-            text3:'Uniquers'
+            text1: 'Welcome',
+            text2: 'to',
+            text3: 'Uniquers'
         };
-        
+
         $scope.gotoHome = function () {
             $timeout(function () {
                 $location.path('/home')
-            },1000);
-        }
+            }, 1000);
+        };
+        $scope.gotoLogin = function () {
+            window.location = window.location.protocol + '//' + window.location.host + "/api/auth/qq";
+        };
     }
 ]);
