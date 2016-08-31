8b2923505541dffce9e874d0fe0df6e5d86b9fca
ChenYue Zhang <CZhang13@slb.com>
2016-08-31 19:39:11 +0800

    make anjular binding

--- database/data.js
+++ database/data.js
@@ -1,279 +1,310 @@
-var db = [
-    {
-        id: 1,
-        type: 'ad',
-        url: '/images/ad1.jpg'
-    },
-    {
-        id: 2,
-        type: 'ad',
-        url: '/images/ad2.jpg'
-    },
-    {
-        id: 3,
-        type: 'ad',
-        url: '/images/ad3.jpg'
-    },
-    {
-        id: 4,
-        type: 'ad',
-        url: '/images/ad4.jpg'
-    },
-    {
-        id: 5,
-        type: 'party',
-        url: '/images/1-1.jpg'
-    },
-    {
-        id: 6,
-        type: 'party',
-        url: '/images/1-2.jpg'
-    },
-    {
-        id: 7,
-        type: 'party',
-        url: '/images/1-3.jpg'
-    },
-    {
-        id: 8,
-        type: 'party',
-        url: '/images/1-4.jpg'
-    },
-    {
-        id: 9,
-        type: 'party',
-        url: '/images/1-5.jpg'
-    },
-    {
-        id: 10,
-        type: 'ol',
-        url: '/images/2-1.jpg'
-    },
-    {
-        id: 11,
-        type: 'ol',
-        url: '/images/2-2.jpg'
-    },
-    {
-        id: 12,
-        type: 'ol',
-        url: '/images/2-3.jpg'
-    },
-    {
-        id: 13,
-        type: 'ol',
-        url: '/images/2-4.jpg'
-    },
-    {
-        id: 14,
-        type: 'ol',
-        url: '/images/2-5.jpg'
-    },
-    {
-        id: 15,
-        type: 'vacation',
-        url: '/images/3-1.jpg'
-    },
-    {
-        id: 16,
-        type: 'vacation',
-        url: '/images/3-2.jpg'
-    },
-    {
-        id: 17,
-        type: 'vacation',
-        url: '/images/3-3.jpg'
-    },
-    {
-        id: 18,
-        type: 'vacation',
-        url: '/images/3-4.jpg'
-    },
-    {
-        id: 19,
-        type: 'vacation',
-        url: '/images/3-5.jpg'
-    },
-    {
-        id: 20,
-        type: 'designer',
-        url: '/images/dwork-01.jpg'
-    },
-    {
-        id: 21,
-        type: 'designer',
-        url: '/images/dwork-02.jpg'
-    },
-    {
-        id: 22,
-        type: 'designer',
-        url: '/images/dwork-03.jpg'
-    },
-    {
-        id: 23,
-        type: 'u1',
-        url: '/images/u1-01.jpg'
-    },
-    {
-        id: 24,
-        type: 'u1',
-        url: '/images/u1-02.jpg'
-    },
-    {
-        id: 25,
-        type: 'u1',
-        url: '/images/u1-03.jpg'
-    },
-    {
-        id: 26,
-        type: 'u1',
-        url: '/images/u1-04.jpg'
-    },
-    {
-        id: 27,
-        type: 'u1',
-        url: '/images/u1-05.jpg'
-    },
-    {
-        id: 28,
-        type: 'u2',
-        url: '/images/u2-01.jpg'
-    },
-    {
-        id: 29,
-        type: 'u2',
-        url: '/images/u2-02.jpg'
-    },
-    {
-        id: 30,
-        type: 'u2',
-        url: '/images/u2-03.jpg'
-    },
-    {
-        id: 31,
-        type: 'u2',
-        url: '/images/u2-04.jpg'
-    },
-    {
-        id: 32,
-        type: 'u3',
-        url: '/images/u3-01.jpg'
-    },
-    {
-        id: 33,
-        type: 'u3',
-        url: '/images/u3-02.jpg'
-    },
-    {
-        id: 34,
-        type: 'u3',
-        url: '/images/u3-03.jpg'
-    },
-    {
-        id: 35,
-        type: 'u3',
-        url: '/images/u3-04.jpg'
-    },
-    {
-        id: 36,
-        type: 'f1',
-        url: '/images/f1-01.jpg'
-    },
-    {
-        id: 37,
-        type: 'f1',
-        url: '/images/f1-02.jpg'
-    },
-    {
-        id: 38,
-        type: 'f1',
-        url: '/images/f1-03.jpg'
-    },
-    {
-        id: 39,
-        type: 'f1',
-        url: '/images/f1-04.jpg'
-    },
-    {
-        id: 40,
-        type: 'f2',
-        url: '/images/f2-01.jpg'
-    },
-    {
-        id: 41,
-        type: 'f2',
-        url: '/images/f2-02.jpg'
-    },
-    {
-        id: 42,
-        type: 'f2',
-        url: '/images/f2-03.jpg'
-    },
-    {
-        id: 43,
-        type: 'f2',
-        url: '/images/f2-04.jpg'
-    },
-     {
-        id: 44,
-        type: 'f3',
-        url: '/images/f3-01.jpg'
-    },
-    {
-        id: 45,
-        type: 'f3',
-        url: '/images/f3-02.jpg'
-    },
-    {
-        id: 46,
-        type: 'f3',
-        url: '/images/f3-03.jpg'
-    },
-    {
-        id: 47,
-        type: 'f3',
-        url: '/images/f3-04.jpg'
-    },
-     {
-        id: 48,
-        type: 'f3',
-        url: '/images/f3-05.jpg'
-    },
-    {
-        id: 49,
-        type: 'f3',
-        url: '/images/f3-06.jpg'
-    },
-    {
-        id: 50,
-        type: 'f3',
-        url: '/images/f3-07.jpg'
-    },
-    {
-        id: 51,
-        type: 'f3',
-        url: '/images/f3-08.jpg'
-    },
-    {
-        id: 52,
-        type: 'u4',
-        url: '/images/u4-01.jpg'
-    },
-    {
-        id: 53,
-        type: 'u4',
-        url: '/images/u4-03.jpg'
-    },
-    {
-        id: 54,
-        type: 'u4',
-        url: '/images/u4-05.jpg'
-    },
-    {
-        id: 55,
-        type: 'u4',
-        url: '/images/u4-07.jpg'
-    }
-];
+var db = [];
 
+db.push({
+    id: db.length,
+    type: 'ad',
+    url: '/images/ad1.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'ad',
+    url: '/images/ad2.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'ad',
+    url: '/images/ad3.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'ad',
+    url: '/images/ad4.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'designer',
+    url: '/images/dwork-01.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'designer',
+    url: '/images/dwork-02.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'designer',
+    url: '/images/dwork-03.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'u1',
+    url: '/images/u1-01.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'u1',
+    url: '/images/u1-02.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'u1',
+    url: '/images/u1-03.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'u1',
+    url: '/images/u1-04.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'u1',
+    url: '/images/u1-05.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'u2',
+    url: '/images/u2-01.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'u2',
+    url: '/images/u2-02.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'u2',
+    url: '/images/u2-03.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'u2',
+    url: '/images/u2-04.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'u2',
+    url: '/images/u2-05.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'u3',
+    url: '/images/u3-01.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'u3',
+    url: '/images/u3-02.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'u3',
+    url: '/images/u3-03.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'u3',
+    url: '/images/u3-04.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'f1',
+    url: '/images/f1-01.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'f1',
+    url: '/images/f1-02.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'f1',
+    url: '/images/f1-03.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'f1',
+    url: '/images/f1-04.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'f2',
+    url: '/images/f2-01.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'f2',
+    url: '/images/f2-02.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'f2',
+    url: '/images/f2-03.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'f2',
+    url: '/images/f2-04.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'f3',
+    url: '/images/f3-01.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'f3',
+    url: '/images/f3-02.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'f3',
+    url: '/images/f3-03.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'f3',
+    url: '/images/f3-04.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'f3',
+    url: '/images/f3-05.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'f3',
+    url: '/images/f3-06.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'f3',
+    url: '/images/f3-07.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'f3',
+    url: '/images/f3-08.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'u4',
+    url: '/images/u4-01.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'u4',
+    url: '/images/u4-02.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'u4',
+    url: '/images/u4-03.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'u4',
+    url: '/images/u4-04.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'u4',
+    url: '/images/u4-05.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'u4',
+    url: '/images/u4-06.jpg',
+    text: ''
+});
+
+db.push({
+    id: db.length,
+    type: 'u4',
+    url: '/images/u4-07.jpg',
+    text: ''
+});
 module.exports = db;
--- public/scripts/controllers/HomeCtrl.js
+++ public/scripts/controllers/HomeCtrl.js
@@ -12,35 +12,59 @@ angular.module('uniquers.controllers').controller('HomeCtrl', [
             })
         }, 0);
 
-        $scope.partyItems = [];
-        $scope.olItems = [];
-        $scope.vacationItems = [];
-        $scope.itemID = '1-1';
-        $scope.itemTypes = {
-            party: 'party',
-            ol: 'ol',
-            vacation: 'vacation',
-        }
+        $scope.adItems = [];
+        $scope.u1Items = [];
+        $scope.u2Items = [];
+        $scope.u3Items = [];
+        $scope.u4Items = [];
+        $scope.f1Items = [];
+        $scope.f2Items = [];
+        $scope.f3Items = [];
 
-        $scope.$on('/home', function () {
-            itemService.getItemsByType('party', function (res) {
-                $scope.partyItems = res;
-            })
+        itemService.getItemsByType('ad', function (res) {
+            for (var i = 0; i < res.length; i++) {
+                if (i == 0) {
+                    res[i].active = 'active';
+                }
+                else {
+                    res[i].active = '';
+                }
+            }
+            $scope.adItems = res;
+        });
 
-            itemService.getItemsByType('ol', function (res) {
-                $scope.olItems = res;
-            })
+        itemService.getItemsByType('u1', function (res) {
+            $scope.u1Items = res;
+        });
 
-            itemService.getItemsByType('vacation', function (res) {
-                $scope.vacationItems = res;
-            })
-        })
+        itemService.getItemsByType('u2', function (res) {
+            $scope.u2Items = res;
+        });
+
+        itemService.getItemsByType('u3', function (res) {
+            $scope.u3Items = res;
+        });
+
+        itemService.getItemsByType('u4', function (res) {
+            $scope.u4Items = res;
+        });
+
+        itemService.getItemsByType('f1', function (res) {
+            $scope.f1Items = res;
+        });
+
+        itemService.getItemsByType('f2', function (res) {
+            $scope.f2Items = res;
+        });
+
+        itemService.getItemsByType('f3', function (res) {
+            $scope.f3Items = res;
+        });
 
         userService.getQQUserInfo('', function (res) {
-            console.log(res);
-              if(res.profile){
-                  $rootScope.profileImgSrc = res.profile._json.figureurl_qq_1;
-              }
+            if (res.profile) {
+                $rootScope.profileImgSrc = res.profile._json.figureurl_qq_1;
+            }
         })
     }
 ]);
