c348d150370b772fca7904d6966f77e5f260cadc
ChenYue Zhang <CZhang13@slb.com>
2016-09-06 11:15:26 +0800

    update detail page

--- public/scripts/controllers/DetailCtrl.js
+++ public/scripts/controllers/DetailCtrl.js
@@ -2,8 +2,19 @@ angular.module('uniquers.controllers').controller('DetailCtrl', [
     '$scope',
     '$rootScope',
     '$routeParams',
+    '$timeout',
     'itemService',
-    function ($scope, $rootScope, $routeParams,itemService) {
+    function ($scope, $rootScope, $routeParams, $timeout,itemService) {
+        $timeout(function () {
+            $('#u-shop-selection-carousel').carousel({
+                interval: 2000
+            })
+
+             $('#u-designer-comment-carousel').carousel({
+                interval: 2000
+            })
+        }, 0);
+
         itemService.getItemById($routeParams.id, function (res) {
             $scope.itemUrl = res[0].url;
         })
