efef540842d9ed5a4ef910d333270fd3ddd1fc64
ChenYue Zhang <CZhang13@slb.com>
2016-09-07 18:42:07 +0800

    add detail

--- public/scripts/controllers/DetailCtrl.js
+++ public/scripts/controllers/DetailCtrl.js
@@ -15,8 +15,20 @@ angular.module('uniquers.controllers').controller('DetailCtrl', [
             })
         }, 0);
 
+        $scope.amount = 1;
+
         itemService.getItemById($routeParams.id, function (res) {
             $scope.itemUrl = res[0].url;
-        })
+        });
+
+        $scope.addAmount = function(){
+            $scope.amount++;
+        }
+
+         $scope.substractAmount = function(){
+            $scope.amount--;
+            if($scope.amount<=0)
+                $scope.amount = 1;
+        }
     }
 ]);
