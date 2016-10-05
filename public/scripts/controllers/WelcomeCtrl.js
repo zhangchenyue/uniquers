cd1de569b4796e4cb8eb16b045fd6bdc7d3e7802
ChenYue Zhang <CZhang13@slb.com>
2016-10-05 17:49:03 +0800

    use js animation

--- public/scripts/controllers/WelcomeCtrl.js
+++ public/scripts/controllers/WelcomeCtrl.js
@@ -6,7 +6,7 @@ angular.module('uniquers.controllers').controller('WelcomeCtrl', [
     'ajaxProxy',
     function ($scope, $rootScope, $timeout, $location, ajaxProxy) {
         $scope.phone =  $rootScope.isPhone();
-        
+
         var t = $rootScope.isPhone() ? 500 : 1000;
         $scope.gotoHome = function () {
             
@@ -14,6 +14,23 @@ angular.module('uniquers.controllers').controller('WelcomeCtrl', [
             // $timeout(function () {
             //     $location.path('/home')
             // }, 500);
+        };
+
+        $scope.onWomanFaceClicked = function(e){
+            $(e.target).fadeOut();
+            $('.face-left > img').fadeIn();
+            $timeout(function () {
+                 $('.face-phone-text img').fadeIn();
+            }, 10);
+           
+        };
+
+        $scope.onManFaceClicked = function(e){
+            $(e.target).fadeOut();
+            $('.face-right > img').fadeIn();
+             $timeout(function () {
+                 $('.face-phone-text img').fadeIn();
+            }, 10);
         };
 
         $scope.gotoLogin = function () {
